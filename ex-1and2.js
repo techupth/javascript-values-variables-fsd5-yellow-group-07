9459d27256b561fa416ae82a4594a21fb6c27a74
BOSS <bigboss1796@hotmail.com>
2023-11-23 14:34:31 +0700

    feat:Values Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let Name = "Karter";
+let Surname = "Anderson";
+let Address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const Restaurant = "Chef's Table by Chef James";
+const netCheck = 15000;
+const openRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netCheck = 5000;
+openRestaurant = 2500;
